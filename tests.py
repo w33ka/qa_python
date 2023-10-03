6c6ac89be56c00248c7cf8fa2ccd49432b80a770
Vika Ananeva <vikalolya@gmail.com>
2023-10-03 12:58:24 +0300

    Исправление tests.py согласно замечаниям: 1.Добавлена параметризация в тест test_add_new_book_without_genre_more_than_40symbols_or_null 2.Добавлен тест test_add_new_book_with_one_or_forty_symbols с параметризацией
    Исправление README.md в связи с добавлением нового теста

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-#проверка на добавление книги без жанра и получение текщего словаря books_genre
+# проверка на добавление книги без жанра и получение текщего словаря books_genre
 def test_add_new_book_without_genre(book_collector):
     book_collector.add_new_book('Гарри Поттер')
 
@@ -9,22 +9,32 @@ def test_add_new_book_without_genre(book_collector):
     assert len(result) == 1
 
 
-#проверка на добавление книги с названием больше 40 символов
-def test_add_new_book_without_genre_more_than_40symbols(book_collector):
-    book_collector.add_new_book('БольшаяКнигаНастолькоЧтоНеУмещаетсяСюдаОй')
+# проверка на добавление книги с названием больше 40 символов или 0 символов
+@pytest.mark.parametrize('book_name', ['', 'БольшаяКнигаНастолькоЧтоНеУмещаетсяСюдаОй'])
+def test_add_new_book_without_genre_more_than_40symbols_or_null(book_collector, book_name):
+    book_collector.add_new_book(book_name)
 
     result = book_collector.get_books_genre()
-    assert 'БольшаяКнигаНастолькоЧтоНеУмещаетсяСюдаОй' not in result
+    assert book_name not in result
 
 
-#добавление книги с жанром который есть в списке genre и проверка жанра книги по имени
+# проверка на добавление книги с названием 40 символов или 1 символом
+@pytest.mark.parametrize('book_name', ['А', 'БольшаяКнигаНастолькоЧтоУмещаетсяСюдаУра'])
+def test_add_new_book_with_one_or_forty_symbols(book_collector, book_name):
+    book_collector.add_new_book(book_name)
+
+    result = book_collector.get_books_genre()
+    assert book_name in result
+
+
+# добавление книги с жанром который есть в списке genre и проверка жанра книги по имени
 def test_set_book_genre(book_collector):
     book_collector.add_new_book('Гарри Поттер')
     book_collector.set_book_genre('Гарри Поттер', 'Фантастика')
     assert book_collector.get_book_genre('Гарри Поттер') == 'Фантастика'
 
 
-#проверка списка книг по выбранному жанру
+# проверка списка книг по выбранному жанру
 
 def test_get_books_with_specific_genre(book_collector):
     book_collector.add_new_book('Гарри Поттер')
@@ -39,7 +49,7 @@ def test_get_books_with_specific_genre(book_collector):
     assert result == ['Гарри Поттер', 'Властелин Колец']
 
 
-#проверка на книги для детей должны быть только с жанром без возрастного рейтинга.
+# проверка на книги для детей должны быть только с жанром без возрастного рейтинга.
 def test_get_books_for_children(book_collector):
     book_collector.add_new_book('Красная шапочка')
     book_collector.add_new_book('Колобок')
@@ -50,6 +60,7 @@ def test_get_books_for_children(book_collector):
     children_book = book_collector.get_books_for_children()
     assert 'Красная шапочка' in children_book and 'Колобок' not in children_book
 
+
 # проверка на добавление книги в избранное из словаря books_genre  и проверка на вывод списка избранного
 def test_add_book_in_favorites(book_collector):
     book_collector.add_new_book('Винни Пух')
@@ -57,6 +68,7 @@ def test_add_book_in_favorites(book_collector):
 
     favorites = book_collector.get_list_of_favorites_books()
     assert 'Винни Пух' in favorites
+
 
 # проверка на добавление книги повторно
 def test_add_book_in_favorites_re_adding_negativ(book_collector):
@@ -66,13 +78,15 @@ def test_add_book_in_favorites_re_adding_negativ(book_collector):
     favorites = book_collector.get_list_of_favorites_books()
     assert favorites.count('Винни Пух') == 1
 
+
 # проверка на добавление книги которой нет в списке books_genre
 def test_add_book_in_favorites_book_not_in_books_genre(book_collector):
     book_collector.add_book_in_favorites('Война и  Мир')
     favorites = book_collector.get_list_of_favorites_books()
     assert 'Война и Мир' not in favorites
 
-#проверка на удаление книги из избранного
+
+# проверка на удаление книги из избранного
 def test_delete_book_from_favorites(book_collector):
     book_collector.add_new_book('Белоснежка и семь гномов')
     book_collector.add_book_in_favorites('Белоснежка и семь гномов')
